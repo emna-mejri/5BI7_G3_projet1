2b4286ca8a8fc9c26c8271e8378481269a4d8082
salhi <aicha.salhi@esprit.tn>
2023-11-09 00:30:43 +0100

    my commit

--- src/main/java/tn/esprit/spring/entities/Registration.java
+++ src/main/java/tn/esprit/spring/entities/Registration.java
@@ -2,7 +2,7 @@ package tn.esprit.spring.entities;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
+import javax.persistence.Entity; 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
